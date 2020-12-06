954c6b1a029281d94d8b72f1246c18a63588ce40
Priyanshi-Sharma-142 <71590973+Priyanshi-Sharma-142@users.noreply.github.com>
2020-12-06 14:10:17 +0530

    Add files via upload

---
+++ SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
